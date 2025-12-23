9d28a3065b0abe0246fe0ccaf634eee049e9c2b3
Pattarapol Tae <pattarapl28.29@gmail.com>
2025-12-23 10:51:51 +0700

    feat: add Tailwind CSS support and Android commands
    - Updated package.json to include new scripts for Android development: `dev:android`, `android:sync`, and `android:run`.
    - Created postcss.config.js to configure PostCSS with Tailwind CSS and autoprefixer.
    - Imported Tailwind CSS styles in main.ts.
    - Added an empty style.css file for Tailwind CSS.
    - Created tailwind.config.js to set up Tailwind CSS configuration.

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,9 @@ import router from './router';
 
 import { IonicVue } from '@ionic/vue';
 
+/* Tailwind CSS */
+import './style.css';
+
 /* Core CSS required for Ionic components to work properly */
 import '@ionic/vue/css/core.css';
 
